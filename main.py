5c3505f59f00b9f37cfc83414ae280a8f24363d9
Kawan S. Dias <113309264+DeadFall323@users.noreply.github.com>
2023-12-29 18:46:18 -0300

    Update main.py
    novas funções e melhor filtragem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random as rd
 import time
+import multiprocessing
 
 
 def digite_senha():
@@ -7,14 +8,32 @@ def digite_senha():
     return senha
 
 
-def cria_senha(tamanho):
-    caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?'
-    senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
-    return senha_final
+def cria_senha(tamanho, complexidade):
+    if complexidade == 0:
+        caracteres = 'abcdefghijklmnopqrstuvwxyz'
+        senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
+        return senha_final
+    elif complexidade == 1:
+        caracteres = '0123456789'
+        senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
+        return senha_final
+    elif complexidade == 2:
+        caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
+        return senha_final
+    elif complexidade == 3:
+        caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
+        return senha_final
+    else:
+        caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?'
+        senha_final = ''.join(rd.choice(caracteres) for _ in range(tamanho))
+        return senha_final
 
 
-def confere_senha(tentativa_senha, senha, arquivo):
+def confere_senha(tentativa_senha, senha, arquivo, tentativas):
     armazena_tentativa(tentativa_senha, arquivo)
+    tentativas.value += 1
     if tentativa_senha == senha:
         print("Senha encontrada com sucesso:", senha)
         return True
@@ -33,25 +52,47 @@ def verifica_tentativa(tentativa, arquivo):
         return tentativa in f.read()
 
 
+def limpar_arquivo(arquivo):
+    with open(arquivo, 'w') as f:
+        f.truncate(0)
+
+
+def tentativa_parallel(senha, arquivo_tentativas, tamanho, complexidade, tentativas):
+    tentativa_senha = cria_senha(tamanho, complexidade)
+    while not confere_senha(tentativa_senha, senha, arquivo_tentativas, tentativas):
+        tentativa_senha = cria_senha(tamanho, complexidade)
+
+
 if __name__ == "__main__":
     senha = digite_senha()
+    print("Digite o nivel de complexidade:")
+    print("0 - Letras a até z")
+    print("1 - Numeros 0 até 9")
+    print("2 - Letras de a até z(Maiúsculas e Minúsculas)")
+    print("3 - Letras de a até z(Maiúsculas e Minúsculas) + Numeros 0 até 9")
+    print("4 - Letras de a até z(Maiúsculas e Minúsculas) + Numeros 0 até 9 + Caracteres especiais ")
+    complexidade = int(input("Digite a complexidade"))
     arquivo_tentativas = 'senhas.txt'
-    tentativa_senha = ''
-    contador_tentativas = 0
+    tamanho_senha = len(senha)
+    tentativas = multiprocessing.Value('i', 0)
+    limpar_arquivo(arquivo_tentativas)
 
     print("Iniciando tentativa de senha")
 
     inicio_tempo = time.time()
 
-    while not confere_senha(tentativa_senha, senha, arquivo_tentativas):
-        tentativa_senha = cria_senha(len(senha))
-        while verifica_tentativa(tentativa_senha, arquivo_tentativas):
-            tentativa_senha = cria_senha(len(senha))
+    processes = [multiprocessing.Process(target=tentativa_parallel,
+                                         args=(senha, arquivo_tentativas, tamanho_senha, complexidade, tentativas)) for
+                 _ in range(4)]
 
-        contador_tentativas += 1
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
 
     fim_tempo = time.time()
     tempo_total = fim_tempo - inicio_tempo
 
     print(f"Tempo total: {tempo_total:.2f} segundos")
-    print(f"Número total de tentativas: {contador_tentativas}")
+    print(f"Número total de tentativas: {tentativas.value}")
